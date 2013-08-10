2242a57d5968fe15c6ab28773733994a24b44846
zzzzzzzzzzzzz <parrot-studio@hotmail.com>
2013-08-10 21:01:17 +0400

    Dynamic pages, first stage

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    it "should have right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
     describe "Help page" do
 
@@ -15,12 +19,20 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+    it "should have right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
   describe "About page" do
     
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+    it "should have right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
