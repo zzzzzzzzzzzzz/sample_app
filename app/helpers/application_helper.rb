345f4a6130beb742c176920113bd517b9a6605eb
zzzzzzzzzzzzz <parrot-studio@hotmail.com>
2013-08-13 12:52:42 +0400

    Stable version with Application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
-  def titler(title)
-    title.empty? ? "Ruby on Rails Tutorial Sample App" : "Ruby on Rails Tutorial Sample App | #{title}" 
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.empty? ? base_title :  "#{base_title} | #{page_title}"
   end
 end
+  
