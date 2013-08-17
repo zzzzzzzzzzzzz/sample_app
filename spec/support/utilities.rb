5edcfd6dc209ec9542b8daad5339ec10bbe18e3d
zzzzzzzzzzzzz <parrot-studio@hotmail.com>
2013-08-17 23:12:30 +0400

    editing-demonstration-deleting

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,10 @@
 include ApplicationHelper
 
 def valid_signin(user) 
+  visit signin_path
   fill_in "Email",    with: user.email.upcase
   fill_in "Password", with: user.password
   click_button "Sign in"
+  # Вход без Capybara.
+  cookies[:remember_token] = user.remember_token
 end
